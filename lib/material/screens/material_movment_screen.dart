a4eadeabfa1f348fc677ee6edc10f332fdb39b74
Loai Judeh <loaidev64@gmail.com>
2025-04-23 11:35:35 +0300

    didn't start with material movement logic

--- lib/material/screens/material_movment_screen.dart
+++ lib/material/screens/material_movment_screen.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/material_movment_controller.dart';
 import 'package:judeh_accounting/shared/widgets/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class MaterialMovmentScreen extends StatelessWidget {
+class MaterialMovmentScreen extends StatefulWidget {
   static const routeName = '/materialMovment';
 
   const MaterialMovmentScreen({super.key});
 
   @override
+  State<MaterialMovmentScreen> createState() => _MaterialMovmentScreenState();
+}
+
+class _MaterialMovmentScreenState extends State<MaterialMovmentScreen> {
+  final controller = Get.put(MaterialMovmentController());
+
+  @override
   Widget build(BuildContext context) {
     return AppScaffold(
-      title: 'materialMovment',
-      child: SliverToBoxAdapter(
+      title: 'حركة المادة',
+      child: SliverPadding(
+          padding: EdgeInsets.symmetric(horizontal: 10.w),
+          sliver: SliverToBoxAdapter(
         child: Text('materialMovment'),
+      ),
       ),
     );
   }
